a51b18a2aec1551a76ebe245f0140c8ed91b95cb
ic1414 <93729382+ic1414@users.noreply.github.com>
2022-02-10 21:12:59 +0000

    Add files via upload

---
+++ examples/ppmESP8266/ppmESP8266.ino
@@ -0,0 +1,20 @@
+#include "ppmESP8266.h"
+
+#define OUTPUT_PIN 10
+
+void setup() {
+  ppmEncoder.begin(OUTPUT_PIN);
+}
+
+void loop() {
+
+  // Min value
+  ppmEncoder.setChannel(0, 500);
+  ppmEncoder.setChannel(0, PPMEncoder::MIN);
+  ppmEncoder.setChannelPercent(0, 0);
+
+  // Max value
+  ppmEncoder.setChannel(0, 2500);
+  ppmEncoder.setChannel(0, PPMEncoder::MAX);
+  ppmEncoder.setChannelPercent(0, 100);
+}
